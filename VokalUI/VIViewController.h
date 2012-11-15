6b9a02ae5e222690055a80ee48be1710cb609249
Anthony Alesia <aalesia88@gmail.com>
2012-11-15 10:03:00 -0600

    VITextView added

--- VokalUI/VIViewController.h
+++ VokalUI/VIViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface VIViewController : UIViewController <UITextFieldDelegate>
+@interface VIViewController : UIViewController <UITextFieldDelegate, UITextViewDelegate>
 
 @end
--- VokalUI/VIViewController.m
+++ VokalUI/VIViewController.m
@@ -8,9 +8,11 @@
 
 #import "VIViewController.h"
 #import "VITextField.h"
+#import "VITextView.h"
 
 @interface VIViewController ()
 @property (weak, nonatomic) IBOutlet VITextField *textField;
+@property (weak, nonatomic) IBOutlet VITextView *textView;
 
 @end
 
@@ -27,8 +29,13 @@
     [_textField setActiveBackgroundImage:[[UIImage imageNamed:@"txt_field_bg_focused.png"]
                           resizableImageWithCapInsets:UIEdgeInsetsMake(12, 12, 12, 12)]];
     
-    _textField.contentOffest = CGPointMake(10.0, 0.0);
+    _textField.contentOffest = CGPointMake(8.0, 0.0);
     _textField.sizeOffset = CGSizeMake(-20.0, 0.0);
+    _textField.placeholder = @"Placeholder";
+    
+    _textView.background = [[UIImage imageNamed:@"txt_field_bg_normal.png"] resizableImageWithCapInsets:UIEdgeInsetsMake(12, 4, 21, 4)];
+    _textView.activeBackground = [[UIImage imageNamed:@"txt_field_bg_focused.png"] resizableImageWithCapInsets:UIEdgeInsetsMake(12, 4, 21, 4)];
+    _textView.placeholder = @"Placeholder";
 }
 
 - (void)didReceiveMemoryWarning
@@ -42,6 +49,18 @@
 - (BOOL)textFieldShouldReturn:(UITextField *)textField
 {
     [textField resignFirstResponder];
+    return YES;
+}
+
+#pragma mark - UITextViewDelegate
+
+- (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text
+{
+    if ([text isEqualToString:@"\n"]) {
+        [textView resignFirstResponder];
+        return NO;
+    }
+    
     return YES;
 }
 
